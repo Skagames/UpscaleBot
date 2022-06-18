c3cf273a1351cbc5b5bf743bb9e6f8728d35cfbe
Skagames <79920562+Skagames@users.noreply.github.com>
2022-06-18 17:28:45 +0200

    update run.bat

--- run.bat
+++ run.bat
@@ -1,5 +1,11 @@
 Rem set the path to the correct location of the bat file
 REM set path=%~dp0
 
+Rem Activate the correct environment
 call %path%/venv/Scripts/activate
+
+Rem go back to the correct location of the bat file
+cd %path%
+
+Rem Run the program
 python main.py
